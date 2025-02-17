e867af50a0eba6f6bc7e3739b32651256e619d9f
coleblond <coltonleblond@gmail.com>
2025-02-16 20:39:01 -0500

    Deleting useless files

--- src/Production.cpp
+++
@@ -1,8 +0,0 @@
-#include "Production.h"
-
-Production::Production(std::string name, std::vector<Token*> firstSet, std::vector<Token*> followSet) :
-	m_name(name), m_firstSet(firstSet), m_followSet(followSet)
-{
-
-
-}
